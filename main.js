36cf4a5603359f1a3e08183477de2425e92ffb1b
* <shaianvaldez@gmail.com>
2021-11-16 06:00:13 +0800

    added welcome channel

--- main.js
+++ main.js
@@ -1136,13 +1136,13 @@ bot.on("guildMemberAdd",(guild,member) => {
      description:`Welcome to ${guild.name}\nEnjoy your stay here and have fun talking with us in <#885495865781944360> or join us in VC`, 
      fields:[{
        name:"RULES", 
-       value:"<a:arrow:906996043051303034>  Read the server rules in\n <#886591125866049597>"
+       value:"<a:arrow:906996043051303034>  Read the server rules in\n <#908978049494704158>"
      },{
        name:"ROLES", 
-       value:"<a:arrow:906996043051303034>  Get your roles in\n <#887161392988311612>"
+       value:"<a:arrow:906996043051303034>  Get your roles in\n <#908980231115788318>"
      },{
        name:"INTRODUCTION", 
-       value:"<a:arrow:906996043051303034>  Introduce yourself in\n<#898573321661210644>"
+       value:"<a:arrow:906996043051303034>  Introduce yourself in\n<#908980116795850772>"
      }], 
      image:{
        url:"https://raw.githubusercontent.com/Maii2603/chill-house-images/main/GlowingPurpleLine-1-1.gif"
